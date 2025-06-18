4af23955cee548bb328f6a690ab0e48be64bfb06
Howard <iamxyzgos5933@gmail.com>
2025-06-18 10:21:17 +0800

    更新完成

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -172,7 +172,7 @@ const disableMagneticButton = () => {
 // 下載按鈕點擊事件
 const handleDownload = (e) => {
   e.preventDefault();
-  window.open('https://mirustory.pse.is/mirums2501', '_blank');
+  window.open('https://drive.usercontent.google.com/download?id=1-M0OpTj-zkz-okjoiZ5T9C75xlnZlo93&export=download&authuser=0', '_blank');
 
   // 按鈕動畫效果
   if (downloadButtonRef.value) {
